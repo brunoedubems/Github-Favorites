1ddde6113bf6944c401ac328451712483ffc2318
Bruno Eduardo <91034858+brunoedubems@users.noreply.github.com>
2022-12-28 16:14:47 -0300

    :)

--- Favorites.js
+++ Favorites.js
@@ -79,6 +79,7 @@ export class FavoritesView extends Favorites {
 
             row.querySelector('.user img').src = `https://github.com/${user.login}.png`;
             row.querySelector('.user img').alt = `Imagem de ${user.name}`
+            row.querySelector('.user a').href = `https://github.com/${user.login}`;
             row.querySelector('.user p').textContent = user.name;
             row.querySelector('.user span').textContent = user.login;
             row.querySelector('.repositories').textContent = user.public_repos;
